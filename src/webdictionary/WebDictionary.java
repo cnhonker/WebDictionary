56c844eafa1df2b66be33eaaa6ce6cfe0138cdd4
Ruiyan Yu <cnhonker.ry@googlemail.com>
2014-07-12 13:50:28 +0200

    Gui Test

--- src/webdictionary/WebDictionary.java
+++ src/webdictionary/WebDictionary.java
@@ -1,5 +1,10 @@
 package webdictionary;
 
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author ry
@@ -7,6 +12,27 @@ package webdictionary;
 public class WebDictionary {
 
     public static void main(String[] args) {
-        
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                createAndShowGui();
+            }
+        });
+    }
+
+    public static void createAndShowGui() {
+        JFrame frame = new JFrame("dict.org");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new JPanel() {
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(300, 300);
+            }
+        });
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
