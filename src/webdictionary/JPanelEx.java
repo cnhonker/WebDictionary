9fc53ecba2fb8f10d3b041f1cc60cb1971ba994f
Ruiyan Yu <cnhonker.ry@googlemail.com>
2014-07-12 14:10:44 +0200

    JPanelEx

---
+++ src/webdictionary/JPanelEx.java
@@ -0,0 +1,15 @@
+package webdictionary;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ry
+ */
+public class JPanelEx extends JPanel{
+
+    public JPanelEx() {
+        add(new JButton("Click"));
+    }
+}
